d7a5d87a4b8dd7c41bbcb303ad066f9a4afe4353
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2024-06-05 20:14:11 -0300

    populate silver_merge bronze_climate

--- elipse-api/database/populate_db.py
+++ elipse-api/database/populate_db.py
@@ -46,14 +46,45 @@ def populate_climate_bronze():
     
     DATA_PATH_2023 = Path(__file__).parents[2] / "data" / "INMET_S_RS_B807_PORTO ALEGRE- BELEM NOVO_01-01-2023_A_31-12-2023.CSV"
     DATA_PATH_2024 = Path(__file__).parents[2] / "data" / "INMET_S_RS_B807_PORTO ALEGRE- BELEM NOVO_01-01-2024_A_29-02-2024.CSV"
+    DATA_PATH_2024_COMPLEMENTARY = Path(__file__).parents[2] / "data" / "COMPLEMENTARY.CSV"
 
+    COLUMN_MAPPING = {
+        'HORA UTC': 'hour_utc',
+        'Data': 'date',
+        'PRECIPITAÇÃO TOTAL, HORÁRIO (mm)': 'total_precip_mm',
+        'PRESSAO ATMOSFERICA AO NIVEL DA ESTACAO, HORARIA (mB)': 'station_pressure_mb',
+        'PRESSÃO ATMOSFERICA MAX.NA HORA ANT. (AUT) (mB)': 'max_pressure_last_hour_mb',
+        'PRESSÃO ATMOSFERICA MIN. NA HORA ANT. (AUT) (mB)': 'min_pressure_last_hour_mb',
+        'RADIACAO GLOBAL (Kj/m²)': 'global_radiation_kj_m2',
+        'TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)': 'air_temp_c',
+        'TEMPERATURA DO PONTO DE ORVALHO (°C)': 'dew_point_temp_c',
+        'TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)': 'max_temp_last_hour_c',
+        'TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)': 'min_temp_last_hour_c',
+        'TEMPERATURA ORVALHO MAX. NA HORA ANT. (AUT) (°C)': 'max_dew_point_last_hour_c',
+        'TEMPERATURA ORVALHO MIN. NA HORA ANT. (AUT) (°C)': 'min_dew_point_last_hour_c',
+        'UMIDADE REL. MAX. NA HORA ANT. (AUT) (%)': 'max_humidity_last_hour_percentage',
+        'UMIDADE REL. MIN. NA HORA ANT. (AUT) (%)': 'min_humidity_last_hour_percentage',
+        'UMIDADE RELATIVA DO AR, HORARIA (%)': 'relative_humidity_percentage',
+        'VENTO, DIREÇÃO HORARIA (gr) (° (gr))': 'wind_direction_deg',
+        'VENTO, RAJADA MAXIMA (m/s)': 'max_wind_gust_m_s',
+        'VENTO, VELOCIDADE HORARIA (m/s)': 'wind_speed_m_s'
+    }
     df_23 = pd.read_csv(DATA_PATH_2023, delimiter=';', skiprows=8, encoding='latin1', decimal=',')
     df_24 = pd.read_csv(DATA_PATH_2024, delimiter=';', skiprows=8, encoding='latin1', decimal=',')
+    df_24_extra = pd.read_csv(DATA_PATH_2024_COMPLEMENTARY, delimiter=';', skiprows=8, encoding='latin1', decimal=',')
 
     df_23.drop(df_23.columns[-1], axis=1, inplace=True) 
     df_24.drop(df_24.columns[-1], axis=1, inplace=True) 
+    df_24_extra.drop(df_24_extra.columns[-1], axis=1, inplace=True)
 
-    df_concat = pd.concat([df_23, df_24], axis=0)
+    df_23.rename(columns=COLUMN_MAPPING, inplace=True)
+    df_24.rename(columns=COLUMN_MAPPING, inplace=True)
+    df_24_extra.rename(columns=COLUMN_MAPPING, inplace=True)
+
+    df_24_extra = df_24_extra.query('1 <= day <= 11 and month == 3')
+    
+    df_concat = pd.concat([df_23, df_24, df_24_extra], axis=0)
+
     df_concat = df_concat.to_dict("records")
 
     response = requests.post(url, json=df_concat)
@@ -61,49 +92,43 @@ def populate_climate_bronze():
     print(response.json())
 
 
-# def preProcess(path):
-#     COLUMN_MAPPING = {
-#     'PRECIPITAÇÃO TOTAL, HORÁRIO (mm)': 'total_precip_mm',
-#     'PRESSAO ATMOSFERICA AO NIVEL DA ESTACAO, HORARIA (mB)': 'station_pressure_mb',
-#     'PRESSÃO ATMOSFERICA MAX.NA HORA ANT. (AUT) (mB)': 'max_pressure_last_hour_mb',
-#     'PRESSÃO ATMOSFERICA MIN. NA HORA ANT. (AUT) (mB)': 'min_pressure_last_hour_mb',
-#     'RADIACAO GLOBAL (Kj/m²)': 'global_radiation_kj_m2',
-#     'TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)': 'air_temp_c',
-#     'TEMPERATURA DO PONTO DE ORVALHO (°C)': 'dew_point_temp_c',
-#     'TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)': 'max_temp_last_hour_c',
-#     'TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)': 'min_temp_last_hour_c',
-#     'TEMPERATURA ORVALHO MAX. NA HORA ANT. (AUT) (°C)': 'max_dew_point_last_hour_c',
-#     'TEMPERATURA ORVALHO MIN. NA HORA ANT. (AUT) (°C)': 'min_dew_point_last_hour_c',
-#     'UMIDADE REL. MAX. NA HORA ANT. (AUT) (%)': 'max_humidity_last_hour_percentage',
-#     'UMIDADE REL. MIN. NA HORA ANT. (AUT) (%)': 'min_humidity_last_hour_percentage',
-#     'UMIDADE RELATIVA DO AR, HORARIA (%)': 'relative_humidity_percentage',
-#     'VENTO, DIREÇÃO HORARIA (gr) (° (gr))': 'wind_direction_deg',
-#     'VENTO, RAJADA MAXIMA (m/s)': 'max_wind_gust_m_s',
-#     'VENTO, VELOCIDADE HORARIA (m/s)': 'wind_speed_m_s'
-#     }
-#     df = pd.read_csv(path, delimiter=';', skiprows=8, encoding='latin1', decimal=',')
-#     df.drop(df.columns[-1], axis=1, inplace=True) # Last column is empty due to ";" at the end of each line
-#     df['Hora UTC'] = df['Hora UTC'].apply(lambda x: datetime.strptime(x, '%H%M %Z')) 
-#     df['Data'] = pd.to_datetime(df['Data'], format='%Y/%m/%d')
-#     df['hour'] = df['Hora UTC'].dt.hour
-#     df['day'] = df['Data'].dt.day
-#     df['month'] = df['Data'].dt.month
-#     df['year'] = df['Data'].dt.year
-#     df.rename(columns=COLUMN_MAPPING, inplace=True)
-#     df.drop(columns=['Data', 'Hora UTC'], axis=1, inplace=True)
-
-#     return df
-
 def populate_silver_merge():
     url = os.environ["API_URL"] + "/database/create/silver_merge"
+    url_climate_bronze = os.environ["API_URL"] + "/database/read_many/climate_bronze?limit=1000000"
+    url_elipse_bronze = os.environ["API_URL"] + "/database/read_many/elipse_bronze?limit=1000000"
+
+    climate_bronze = requests.get(url_climate_bronze).json()
+    elipse_bronze = requests.get(url_elipse_bronze).json()
+
+    climate_bronze = pd.DataFrame(climate_bronze)
+    elipse_bronze = pd.DataFrame(elipse_bronze)
     
-    DATA_PATH_2023 = Path(__file__).parents[2] / "data" / "INMET_S_RS_B807_PORTO ALEGRE- BELEM NOVO_01-01-2023_A_31-12-2023.CSV"
-    DATA_PATH_2024 = Path(__file__).parents[2] / "data" / "INMET_S_RS_B807_PORTO ALEGRE- BELEM NOVO_01-01-2024_A_29-02-2024.CSV"
+    climate_bronze['hour_utc'] = climate_bronze['hour_utc'].apply(lambda x: datetime.strptime(x, '%H%M %Z')) 
+    climate_bronze['date'] = pd.to_datetime(climate_bronze['date'], format='%Y/%m/%d')
+    climate_bronze['hour'] = climate_bronze['hour_utc'].dt.hour
+    climate_bronze['day'] = climate_bronze['date'].dt.day
+    climate_bronze['month'] = climate_bronze['date'].dt.month
+    climate_bronze['year'] = climate_bronze['date'].dt.year
+    climate_bronze.drop(columns=['date', 'hour_utc'], axis=1, inplace=True)
 
-    df23, df24 = preProcess(DATA_PATH_2023), preProcess(DATA_PATH_2024)
-    df_23_24_concat = pd.concat([df23, df24], axis=0)
+    elipse_bronze['timestamp'] = pd.to_datetime(elipse_bronze['timestamp'], format='%Y-%m-%d %H:%M:%S')
+    elipse_bronze['hour'] = elipse_bronze['timestamp'].dt.hour
+    elipse_bronze['day'] = elipse_bronze['timestamp'].dt.day
+    elipse_bronze['month'] = elipse_bronze['timestamp'].dt.month
+    elipse_bronze['year'] = elipse_bronze['timestamp'].dt.year
 
-
+    silver_merge = elipse_bronze.merge(climate_bronze, on=['hour', 'day', 'year', 'month'], how='left') 
+    silver_merge['global_radiation_kj_m2'] = silver_merge['global_radiation_kj_m2'].fillna(0)
+    silver_merge = silver_merge.ffill()
+    silver_merge.drop(columns=['day', 'month', 'hour', 'year'], inplace=True)
+    
+    silver_merge = silver_merge.to_dict("records")
+    
+    response = requests.post(url, json=silver_merge)
+    print(response.status_code)
+    print(response.json())
+    
 if __name__ == "__main__":
     populate_elipse_bronze()
+    populate_climate_bronze()
     populate_silver_merge()
